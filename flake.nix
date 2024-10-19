4405cbca11193f6f78a725eeead6ee53fa430921
adjoly <adamjly@proton.me>
2024-10-19 12:13:45 +0200

    「✨」 feat(Flake): Added flake for easier developement

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+	description = "A Nix-flake-based C/C++ development environment";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+		let 
+			pkgs = nixpkgs.legacyPackages.${system};
+		in
+		{
+			devShells = ({
+			    default = pkgs.mkShell.override
+				{
+					# For stdenv
+				}
+				{
+					buildInputs = with pkgs; [
+						SDL2
+						vulkan-loader
+					];
+
+					LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib";
+
+					packages = with pkgs; [
+						clang_12
+						gcc11
+						gdb
+						norminette
+						valgrind
+					];
+				};
+			});
+    });
+}
