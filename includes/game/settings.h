cf80c34f41f83d2847f5de5f4214a0e6dca1b8ee
Maelys <maelysdumergue@gmail.com>
2024-11-06 14:10:59 +0100

    speed

--- includes/game/settings.h
+++ includes/game/settings.h
@@ -6,7 +6,7 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:06:39 by adjoly            #+#    #+#             */
-/*   Updated: 2024/11/06 13:19:25 by adjoly           ###   ########.fr       */
+/*   Updated: 2024/11/06 14:02:22 by madumerg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 # define WINDOW_W 1600
 # define WINDOW_H 900
 # define PLAYER_ROT_SPEED 0.034906585039887
-# define PLAYER_SPEED 1.5
+# define PLAYER_SPEED 3
 # define FOV 70
 
 # define CHUNK_SIZE 64
